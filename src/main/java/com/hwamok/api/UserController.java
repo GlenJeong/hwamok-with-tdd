21c8e8e0607b0cdcc5c2a48afde87f309d974eba
Glenn Jeong <jyb1624@gmail.com>
2023-12-28 14:52:30 +0900

    commit-git 파일 업로드 테스트

--- src/main/java/com/hwamok/api/UserController.java
+++ src/main/java/com/hwamok/api/UserController.java
@@ -76,3 +76,47 @@ public class UserController {
     }
 
 }
+
+// @PostMapping
+//    public ResponseEntity<ApiResult<?>> create(@RequestBody FileCreateDto.Request request) {
+//        fileService.upload(request.getImageProfile());
+//        return Result.created();
+//    }
+// 문제의 원인은 @RequestBody를 사용할 때 기본적으로 Content-Type이 application/json으로 설정되기 때문입니다.
+// 하지만 파일을 업로드할 때는 multipart/form-data로 설정되어야 합니다.
+// 따라서 @RequestBody를 사용할 때는 자동으로 파싱되는 Content-Type이 multipart/form-data가 아니라서 해당 에러가 발생하는 것입니다.
+// @RequestPart는 Spring에서 멀티파트 요청에서 파일이나 파라미터를 추출하는 데 사용되는 애노테이션입니다.
+// 주로 파일 업로드와 함께 사용되며, @RequestBody와 유사하지만 멀티파트 요청에서 데이터를 추출하는 데 특화되어 있습니다.
+// 멀티파트 요청은 보통 파일 업로드와 관련이 있습니다. HTML 폼에서 enctype="multipart/form-data"를 사용하거나,
+// REST 클라이언트에서 파일을 첨부하여 요청할 때 사용됩니다. @RequestPart를 사용하면 멀티파트 요청에서 파일과 함께 전송되는
+// 다른 파라미터들을 추출할 수 있습니다.
+
+//문제가 발생하는 이유는 @RequestPart를 사용할 때 클라이언트에서 전송되는 멀티파트 요청의 구조와 서버에서 처리하는 메서드의
+// 파라미터 구조가 일치하지 않기 때문입니다.
+//첫 번째 코드에서는 다음과 같이 작성되어 있습니다.
+//@PostMapping
+//public ResponseEntity<ApiResult<?>> create(@RequestPart FileCreateDto.Request request) {
+//    fileService.upload(request.getImageProfile());
+//    return Result.created();
+//}
+//여기서 @RequestPart FileCreateDto.Request request는 FileCreateDto.Request 객체를 요청의 일부로 받아들이려고 시도합니다.
+// 그러나 클라이언트에서 실제로는 FileCreateDto.Request 객체의 imageProfile 필드만을 멀티파트로 보내고 있습니다.
+// 그 결과로 서버에서는 FileCreateDto.Request 객체를 바인딩하는 데 실패하고, 요청에 필요한 request 파라미터가 없다는 에러가 발생하게 됩니다.
+//두 번째 코드에서는 다음과 같이 작성되어 있습니다.
+//@PostMapping
+//public ResponseEntity<ApiResult<?>> create(@RequestPart MultipartFile imageProfile) {
+//    fileService.upload(imageProfile);
+//    return Result.created();
+//}
+//여기서는 @RequestPart MultipartFile imageProfile로 직접 파일만을 받아들이고 있습니다. 따라서 클라이언트에서도 멀티파트로 전송되는 것은
+// 파일 하나 뿐이므로 일치하게 됩니다.
+//만약 첫 번째 코드처럼 객체를 통째로 받고 싶다면 클라이언트에서도 객체 전체를 멀티파트로 보내야 합니다.
+// 클라이언트 측에서는 멀티파트 요청을 구성하여 서버로 전송해야 합니다. 일반적으로 HTML 폼에서 파일 업로드를 하는 경우에 해당하는데,
+// REST 클라이언트에서는 파일을 첨부하여 요청하는 방식으로 구현할 수 있습니다.
+// 클라이언트 코드를 확인하고, 필요에 따라 서버 측 코드를 조정하여 클라이언트와 서버 간의 요청 및 응답 구조를 일치시키세요.
+
+// 문제는 @PatchMapping("/{id}")에서 @RequestPart MultipartFile profilePicture를 받고 있기 때문에 발생하고 있는 것 같습니다.
+// @PatchMapping을 사용할 때는 Spring에서는 기본적으로 Multipart 요청을 지원하지 않습니다.
+// Multipart 요청은 주로 파일 업로드와 관련이 있기 때문에 이를 지원하지 않는 경우도 있습니다.
+// PATCH 메서드는 JSON 형식의 데이터를 전송하며, 파일 업로드와 관련된 경우가 아니라면 Multipart 요청을 사용하지 않습니다.
+// 따라서 PATCH 메서드에서는 @RequestBody를 사용하여 JSON 데이터를 받는 것이 일반적입니다.
--- src/test/java/com/hwamok/api/AuthControllerTest.java
+++ src/test/java/com/hwamok/api/AuthControllerTest.java
@@ -1,5 +1,8 @@
 package com.hwamok.api;
 
+import com.epages.restdocs.apispec.ResourceDocumentation;
+import com.epages.restdocs.apispec.ResourceSnippetParametersBuilder;
+import com.epages.restdocs.apispec.Schema;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hwamok.api.dto.auth.LoginDto;
 import com.hwamok.user.domain.User;
@@ -11,13 +14,19 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.restdocs.payload.PayloadDocumentation;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -92,7 +101,33 @@ class AuthControllerTest {
                         jsonPath("message").value("success"),
                         jsonPath("data.accessToken").isNotEmpty(),
                         jsonPath("data.refreshToken").isNotEmpty()
-                );
+                )
+                .andDo(document("/auth/login",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        ResourceDocumentation.resource(
+                                new ResourceSnippetParametersBuilder()
+                                        .tag("Auth")
+                                        .description("로그인 API")
+                                        .requestFields(
+                                                List.of(
+                                                        PayloadDocumentation.fieldWithPath("loginId").type(JsonFieldType.STRING).description("jiy88226"),
+                                                        PayloadDocumentation.fieldWithPath("password").type(JsonFieldType.STRING).description("q1w2e3r4t5")
+                                                        )
+                                        )
+                                        .responseFields(
+                                                List.of(
+                                                        PayloadDocumentation.fieldWithPath("code").type(JsonFieldType.STRING).description("S000"),
+                                                        PayloadDocumentation.fieldWithPath("message").type(JsonFieldType.STRING).description("success"),
+                                                        PayloadDocumentation.fieldWithPath("data.accessToken").type(JsonFieldType.STRING).description("access Token"),
+                                                        PayloadDocumentation.fieldWithPath("data.refreshToken").type(JsonFieldType.STRING).description("refresh Token")
+                                                        )
+                                        )
+                                        .requestSchema(Schema.schema("LoginDto.Request"))
+                                        .responseSchema(Schema.schema("LoginDto.Response"))
+                                        .build()
+                        )
+                ));
 //                .andDo(document("/auth/login",
 //                        preprocessRequest(prettyPrint()),
 //                        preprocessResponse(prettyPrint())
@@ -116,5 +151,14 @@ class AuthControllerTest {
                         jsonPath("data.accessToken").isNotEmpty(),
                         jsonPath("data.refreshToken").isNotEmpty()
                 );
+
+//        resultActions.andDo(
+//                        DocsUtil.createDocs(
+//                                "Auth2",
+//                                "Auth/login2",
+//                                "로그인 자동화 API",
+//                                resultActions
+//                                )
+//                );
     }
 }
