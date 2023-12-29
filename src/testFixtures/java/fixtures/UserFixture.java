bb5057e1a46ef2c2f94602c313ae97eee28135fa
Glenn Jeong <jyb1624@gmail.com>
2023-12-29 11:53:13 +0900

    commit-git 파일 업로드 테스트 5

--- src/testFixtures/java/fixtures/UserFixture.java
+++ src/testFixtures/java/fixtures/UserFixture.java
@@ -2,7 +2,6 @@ package fixtures;
 
 import com.hwamok.user.domain.User;
 public class UserFixture {
-
     public static User createUser(String loginId, String password) throws Exception {
         return new User(loginId, password, "jyb1624@test.com", "Glenn", "정인범", "ACTIVATED", "1988-02-26");
     }
